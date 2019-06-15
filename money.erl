60f65929f46558a95c5404d832fbba79264efc44
ROHAN DEEPAK PASPALLU <paspallu.rohan@gmail.com>
2019-06-14 22:13:31 -0400

    changes in money file

--- money.erl
+++ money.erl
@@ -9,35 +9,40 @@
 main_function() ->
 	
 	{_,Cdata} = file:consult("customers.txt"),
-%%  	io:format("~p~n", [Cdata]),
 	
 	{_,Bdata} = file:consult("banks.txt"),
-%%  	io:format("~p~n", [Bdata]),
 	
 	Customer_map = maps:from_list(Cdata),
-%% 	io:format("~p~n", [Customer_map]),
 	
 	Bank_map = maps:from_list(Bdata),
-%% 	io:format("~p~n", [Bank_map]),
 		
-%% 	io:fwrite("~p ~p~n",[maps:keys(Customer_map), maps:values(Customer_map)]),
+	Random_Amt = 0,
 	
 	
-	
-	
-	customer_calling(Cdata,Bdata,Customer_map,Bank_map).
+	customer_calling(Cdata,Bdata,Customer_map,Bank_map,Random_Amt).
 
 
-customer_calling(Cdata,Bdata,Customer_map,Bank_map) ->
+customer_calling(Cdata,Bdata,Customer_map,Bank_map,Random_Amt) ->
 	
-%% 	io:fwrite("inside customer calling ~n"),
+
 	Size_customer = maps:size(Customer_map),
 	C_key = maps:keys(Customer_map),
+	
+	C_values = maps:values(Customer_map),
+	
+%% 	lists:foreach(fun(Key)->
+%% 						  #{Key := Val} = Customer_map,
+%% 						  if
+%% 							  Val ==0 ->
+%% 								  Customer_map10 = maps:remove(Key, Customer_map),
+%% 								  customer_calling(Cdata, Bdata, Customer_map10, Bank_map, Random_Amt)
+%% 						  end
+%% 				  
+%% 				  end, C_key),
 	Random_customer_index = rand:uniform(Size_customer),
 	Random_customer_key = lists:nth(Random_customer_index, C_key),
 	Random_customer_value = maps:get(Random_customer_key,Customer_map),
-%% 	io:fwrite("~p~n",[Random_customer_key]),
-%% 	io:fwrite("~p ~p~n",[Random_customer_key,maps:get(Random_customer_key,Customer_map)]),
+
 	
 	
 	Size_bank = maps:size(Bank_map),
@@ -45,10 +50,13 @@ customer_calling(Cdata,Bdata,Customer_map,Bank_map) ->
 	Random_bank_Index = rand:uniform(Size_bank),
 	Random_bank_key = lists:nth(Random_bank_Index, B_key),
 	Random_bank_value = maps:get(Random_bank_key,Bank_map),
-%% 	io:fwrite("~p~n",[Random_bank_key]),
-%% 	io:fwrite("~p ~p~n",[Random_bank_key,maps:get(Random_bank_key,Bank_map)]),
 	
-	Random_Amt = rand:uniform(50),	
+	
+
+	
+%% 	Random_Amt = rand:uniform(50),	
+	
+%% 	Random_Amt = 0,
 	
 	lists:foreach(fun(Key)->
 						  #{Key := Val} = Customer_map,
@@ -62,7 +70,7 @@ get_feedback() ->
 	receive
 		{[Cdata],[Bdata],[Customer_map],[Bank_map],[Size_customer],[Size_bank],[C_key],[B_key],[Random_customer_index],[Random_bank_Index],[Random_customer_key],[Random_bank_key],[Random_customer_value],[Random_bank_value],[Random_Amt]} -> 
 
-%%  			io:format("~s having loan amount ~p requests a loan of ~p dollar(s) from ~s having funds ~p \n\n", [Random_customer_key,Random_customer_value,Random_Amt,Random_bank_key, Random_bank_value]),
+%%   			io:format("~s having loan amount ~p requests a loan of ~p dollar(s) from ~s having funds ~p \n\n", [Random_customer_key,Random_customer_value,Random_Amt,Random_bank_key, Random_bank_value]),
 
 
 			
@@ -75,10 +83,10 @@ get_feedback() ->
 
 
 
+
+
 bank_calling(Cdata,Bdata,Customer_map,Bank_map,Size_customer,Size_bank,C_key,B_key,Random_customer_index,Random_bank_Index,Random_customer_key,Random_bank_key,Random_customer_value,Random_bank_value,Random_Amt) ->
 	
-	
-%% 	io:fwrite("inside bank calling ~n"),
 	
 	
 %%  	io:format("~s having loan amount ~p requests a loan of ~p dollar(s) from ~s having funds ~p \n\n", [Random_customer_key,Random_customer_value,Random_Amt,Random_bank_key, Random_bank_value]),
@@ -99,7 +107,7 @@ get_bank_feedback() ->
 			
 %% 			io:fwrite("~s having funds ~p approves a loan of ~p dollars from ~s remaining balance ~p~n",[Random_bank_key, Random_bank_value, Random_Amt, Random_customer_key, Random_customer_value]),
 			
-			customer_calling(Cdata, Bdata, Customer_map, Bank_map)
+			customer_calling(Cdata, Bdata, Customer_map, Bank_map,Random_Amt)
 			
 	after 2000 -> true
 
